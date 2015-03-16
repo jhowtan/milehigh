de123446027d7fab4dcec7c754313efd21adf0df
martinyim <martinywm@gmail.com>
2015-03-16 14:55:40 +0800

    Add admin page

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+<body class="soap-login-page style1 body-blank">
+    <div id="page-wrapper" class="wrapper-blank">
+        <section id="content">
+            <div class="container">
+                <div id="main">
+                    <h1 class="logo block">
+                        <a href="index.html" title="Travelo - home">
+                            <img src="../images/logo2.png" alt="Travelo HTML5 Template" />
+                        </a>
+                    </h1>
+                    <div class="text-center yellow-color box" style="font-size: 4em; font-weight: 300; line-height: 1em;">Admin Page</div>
+                    <p class="light-blue-color block" style="font-size: 1.3333em;">Login to Admin account</p>
+                    <div class="col-sm-8 col-md-6 col-lg-5 no-float no-padding center-block">
+                        <form class="login-form">
+                            <div class="form-group">
+                                <input type="text" class="input-text input-large full-width" placeholder="enter your email or username">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="input-text input-large full-width" placeholder="enter your password">
+                            </div>
+                            <button type="submit" class="btn-large full-width sky-blue1">LOGIN</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
