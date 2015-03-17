eacdba6a9de9c48ee412d5b22cbbc2a1f24e6919
Jonathan Tan <jhow.tan@gmail.com>
2015-03-17 16:23:31 +0800

    Provide instructions for config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
-$db_user = "db_login_username";
-$db_pass = "db_login_password";
-$db_host = "localhost";
+// DO NOT COMMIT CHANGES TO THIS FILE
+$db_user = "db_login_username"; // note different users for development and production
+$db_pass = "db_login_password"; // note different passwords for your user
+$db_host = "localhost:3306"; // on development use localhost:8889
 $db_name = "cs2102";
 ?>
