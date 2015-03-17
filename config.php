82f5007ef1ae885ba3f4443d31372f9ff98c7c7a
sofox <darylhzw@gmail.com>
2015-03-17 16:54:10 +0800

    Add config file to ignore

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // DO NOT COMMIT CHANGES TO THIS FILE
-$db_user = "db_login_username"; // note different users for development and production
-$db_pass = "db_login_password"; // note different passwords for your user
-$db_host = "localhost:3306"; // on development use localhost:8889
+$db_user = "root"; // note different users for development and production
+$db_pass = "password"; // note different passwords for your user
+$db_host = "localhost"; // on development use localhost:8889
 $db_name = "cs2102";
 ?>
